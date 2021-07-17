adba19e5a0a53e1fbb0f7ec7ea8a542d76740f0d
hayoung <gksyddl22@gmail.com>
2021-07-17 09:42:35 +0900

    node - 내장 객체 사용해보기 (스탠다드)

--- src/node/main.js
+++ src/node/main.js
@@ -14,5 +14,9 @@
 // node standard library에 있는 모듈이나 node_modules안에 있는 모듈은 절대경로를 지정해 가져온다.
 // 절대경로를 지정하면 module.paths의 경로들을 순서대로 검사하여 해당 모듈이 있으면 가장 첫 번째 것을 가져온다.
 
-const animals = require('./animals')
-console.log(animals)
+process.stdin.setEncoding('utf-8')
+process.stdin.on('data', (data) => {
+  console.log(data, data.length)
+})
+
+process.stdin.pipe(process.stdout)
